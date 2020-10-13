67024fbad71fdf71dbbafa84b92eda524e52ed67
Amine AMEZIANE <amezianeamine@gmail.com>
2020-10-13 06:43:39 +0100

    Container declaration removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Dashboard from "./views/Dashboard";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Container, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
